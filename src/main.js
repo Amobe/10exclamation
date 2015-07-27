f3203df04d112cad38171799300d628e2b09eb05
Lexamo <amobe.momoko@gmail.com>
2015-07-27 23:48:07 +0800

    make up a container contain 5*5 image each image has the onClick event to trun the rotation on/off

---
+++ src/main.js
@@ -0,0 +1,49 @@
+var stage = new PIXI.Stage(0x66FF99);
+
+var renderer = PIXI.autoDetectRenderer(800, 600, {
+	backgroundColor: 0x666666
+});
+
+document.body.appendChild(renderer.view);
+
+requestAnimationFrame(animate);
+
+var container = new PIXI.Container();
+stage.addChild(container);
+
+for (var j = 0; j < 5; ++j) {
+	for (var i = 0; i < 5; ++i) {
+		var texture = PIXI.Texture.fromImage("image/square.jpg");
+		var square = new PIXI.Sprite(texture);
+
+		square.anchor = new PIXI.Point(0.5, 0.5);
+		square.scale = new PIXI.Point(0.1, 0.1);
+		square.position = new PIXI.Point(40*i, 40*j);
+
+		// click event
+		square.interactive = true;
+		square.on("mousedown", onClick);
+		square.on("touchstart", onClick);
+		square.isClick = false;
+
+		container.addChild(square);
+	}
+}
+
+container.position.x = 400;
+container.position.y = 300;
+
+function animate() {
+	requestAnimationFrame(animate);
+	for (var i = 0; i < container.children.length; ++i) {
+		var square = container.children[i];
+		if (square.isClick)
+			square.rotation += 0.1;
+	}
+
+	renderer.render(stage);
+}
+
+function onClick(eventData) {
+	eventData.target.isClick = !eventData.target.isClick;
+}
